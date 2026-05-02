68181beefc506391f3640ff2db9c3ad9dc1f121c
Franz Keilholz <franz@keilholz.biz>
2026-05-03 00:38:46 +0200

    renamed variables to match nomenclature

--- lib/workbooks/F42_Beltz_TT_Zwangsstörungen_Fricke.py
+++ lib/workbooks/F42_Beltz_TT_Zwangsstörungen_Fricke.py
@@ -6,7 +6,7 @@ logger = get_logger(__name__)
 
 
 def F42_extract_working_sheet_numbers_names(doc: pymupdf.Document, overview_pages: list[int]) -> dict[int, str]:
-    working_papaer_numbers_names = {}
+    working_sheet_numbers_names = {}
     pattern = re.compile(r"AB\s+(\d+)\s*/\s*(.*)", re.IGNORECASE)
 
     for page_num in overview_pages:
@@ -16,13 +16,13 @@ def F42_extract_working_sheet_numbers_names(doc: pymupdf.Document, overview_page
         for line in text.splitlines():
             match = pattern.search(line)
             if match:
-                ab_num = int(match.group(1))
-                ab_name = match.group(2).strip()
-                ab_name = ab_name.replace("?", "")
-                working_papaer_numbers_names[ab_num] = ab_name
-                logger.debug(f"AB {ab_num} -> {ab_name}")
+                sheet_num = int(match.group(1))
+                sheet_name = match.group(2).strip()
+                sheet_name = sheet_name.replace("?", "")
+                working_sheet_numbers_names[sheet_num] = sheet_name
+                logger.debug(f"AB {sheet_num} -> {sheet_name}")
 
-    return working_papaer_numbers_names
+    return working_sheet_numbers_names
 
 def F42_extract_working_sheet_pages(doc: pymupdf.Document) -> dict[int, list[int]]:
     working_sheets_pages = defaultdict(list)
@@ -34,6 +34,6 @@ def F42_extract_working_sheet_pages(doc: pymupdf.Document) -> dict[int, list[int
         match = pattern.search(header_text)
 
         if match:
-            ab_num = int(match.group(1))
-            working_sheets_pages[ab_num].append(page_num)
+            sheet_num = int(match.group(1))
+            working_sheets_pages[sheet_num].append(page_num)
     return working_sheets_pages
