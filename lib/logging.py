2118c964c4d2eeeed5d98b0c470bd05b97ae75f3
Franz Keilholz <franz@keilholz.biz>
2026-05-03 15:11:11 +0200

    renamed variables

--- lib/logging.py
+++ lib/logging.py
@@ -1,18 +1,17 @@
 import logging
 import sys
 
-_FMT = "%(asctime)s | %(levelname)s | %(name)s:%(lineno)d | %(funcName)s | %(message)s"
-_FORMATTER = logging.Formatter(_FMT)
+FMT = "%(asctime)s | %(levelname)s | %(name)s:%(lineno)d | %(funcName)s | %(message)s"
+FORMATTER = logging.Formatter(FMT)
 
 
 def setup_logger(console_level: int = logging.INFO, root_level: int = logging.DEBUG) -> None:
-
     root = logging.getLogger()
 
     root.setLevel(root_level)
 
     handler = logging.StreamHandler(sys.stdout)
-    handler.setFormatter(_FORMATTER)
+    handler.setFormatter(FORMATTER)
     handler.setLevel(console_level)
     root.addHandler(handler)
 
